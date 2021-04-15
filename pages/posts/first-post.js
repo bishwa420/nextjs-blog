9671ed28440205258361d044a998e476b20ebd55
Bishwajit <bishwa420@gmail.com>
2021-04-15 18:06:20 +0600

    - Link example added - Profile photo added

---
+++ pages/posts/first-post.js
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export default function FirstPost() {
+
+    return (
+     <>
+        <h1>First Post</h1>
+         <h2>
+             <Link href="/">
+                 <a>Back to Home</a>
+             </Link>
+         </h2>
+
+         <Image
+             src="/images/profile.jpg"
+             height = {144}
+             width = {144}
+             alt = "Bishwajit"
+         />
+     </>
+    )
+}
